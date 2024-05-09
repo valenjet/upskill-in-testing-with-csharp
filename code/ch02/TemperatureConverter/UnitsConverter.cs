ebd214f52439f45884c69e62181972e2d3c673d5
Stephen Ritchie <ruthlesshelp@gmail.com>
2024-05-09 08:57:05 -0400

    Renaming namespaces, classes, and methods to match manuscript text.

--- code/ch02/TemperatureConverter/UnitsConverter.cs
+++ code/ch02/TemperatureConverter/UnitsConverter.cs
@@ -1,6 +1,6 @@
-﻿namespace Temperature.Helpers;
+﻿namespace Physics.Temperature;
 
-public class UnitsConverter
+public class ScaleConverter
 {
     public int FahrenheitToCelsius(int value){
         throw new NotImplementedException("Not yet started!");
